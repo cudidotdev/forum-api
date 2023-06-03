f439cc032340536b8f5d730c4f8c6336bcd8d17a
Cudilala <augustinemadu9@gmail.com>
2023-06-03 11:30:17 +0100

    create post api

--- src/api/posts/controllers.rs
+++ src/api/posts/controllers.rs
@@ -1,15 +1,28 @@
 use actix_web::{web, HttpResponse};
-use deadpool_postgres::{Client, Pool};
+use deadpool_postgres::Pool;
 use serde_json::json;
 
-use super::models;
+use super::models::{self, NoDBClient, NoUserDetails, NotFormated};
 use crate::api::UserAuth;
 
 pub async fn create_post(
-  user_detail: UserAuth,
+  user_details: UserAuth,
   db_pool: web::Data<Pool>,
-  body: web::Json<models::CreatePostDetails>,
+  body: web::Json<models::CreatePostDetails<NoDBClient, NoUserDetails, NotFormated>>,
 ) -> HttpResponse {
+  if user_details.details.is_none() {
+    return HttpResponse::Forbidden().json(json!({
+      "success": false,
+      "message": "User not signed in",
+      "error": {
+        "name": "re-auth",
+        "message": "User not signed in"
+      }
+    }));
+  };
+
+  let user_details = user_details.details.unwrap();
+
   let db_client_res = db_pool.get().await;
 
   if let Err(e) = db_client_res {
@@ -21,15 +34,26 @@ pub async fn create_post(
 
   let db_client = db_client_res.unwrap();
 
-  let res = body.into_inner().add_db_client(&db_client).create_post();
+  let res = body
+    .into_inner()
+    .format()
+    .add_db_client(&db_client)
+    .add_user_details(&user_details)
+    .create_post()
+    .await;
 
-  if let Err(e) = res {
-    return HttpResponse::Ok().status(e.0).json(json!({
+  match res {
+    Ok(id) => HttpResponse::Ok().json(json!({
+      "success": true,
+      "data": {
+        "id": id
+      }
+    })),
+
+    Err((s, v)) => HttpResponse::Ok().status(s).json(json!({
       "success": false,
-      "message": e.1["message"],
-      "error": e.1
-    }));
+      "message": v["message"],
+      "error": v
+    })),
   }
-
-  HttpResponse::Ok().finish()
 }
--- src/api/posts/models.rs
+++ src/api/posts/models.rs
@@ -1,38 +1,95 @@
+use std::marker::PhantomData;
+
+use crate::api::UserAuthDetails;
 use actix_web::http::StatusCode;
 use deadpool_postgres::Client;
+use lazy_static::lazy_static;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
 #[derive(Serialize, Deserialize)]
-pub struct CreatePostDetails {
-  title: Option<String>,
-  topics: Option<Vec<String>>,
-  body: Option<String>,
+pub struct CreatePostDetails<D, U, F> {
+  title: String,
+  topics: Vec<String>,
+  body: String,
+  #[serde(skip_deserializing)]
+  db_client: D,
+  #[serde(skip_deserializing)]
+  user_details: U,
+  #[serde(skip_deserializing)]
+  formatted: PhantomData<F>,
 }
+#[derive(Default)]
+pub struct NoDBClient;
+pub struct DBClient<'a>(&'a Client);
+#[derive(Default)]
+pub struct NoUserDetails;
+pub struct UserDetails<'a>(&'a UserAuthDetails);
+#[derive(Default)]
+pub struct NotFormated;
+pub struct Formatted;
 
-pub struct CreatePostDetailsWithDBClient<'a> {
-  title: Option<String>,
-  topics: Option<Vec<String>>,
-  body: Option<String>,
-  db_client: &'a Client,
-}
-
-impl CreatePostDetails {
-  pub fn add_db_client(self, db_client: &Client) -> CreatePostDetailsWithDBClient {
-    CreatePostDetailsWithDBClient {
+impl<U, F> CreatePostDetails<NoDBClient, U, F> {
+  pub fn add_db_client(self, db_client: &Client) -> CreatePostDetails<DBClient, U, F> {
+    CreatePostDetails {
       title: self.title,
       topics: self.topics,
       body: self.body,
-      db_client: db_client,
+      db_client: DBClient(db_client),
+      user_details: self.user_details,
+      formatted: PhantomData,
     }
   }
 }
 
-impl<'a> CreatePostDetailsWithDBClient<'a> {
-  pub fn create_post(&self) -> Result<(), (StatusCode, Value)> {
+impl<D, F> CreatePostDetails<D, NoUserDetails, F> {
+  pub fn add_user_details(
+    self,
+    user_details: &UserAuthDetails,
+  ) -> CreatePostDetails<D, UserDetails, F> {
+    CreatePostDetails {
+      title: self.title,
+      topics: self.topics,
+      body: self.body,
+      db_client: self.db_client,
+      user_details: UserDetails(user_details),
+      formatted: PhantomData,
+    }
+  }
+}
+
+impl<D, U> CreatePostDetails<D, U, NotFormated> {
+  pub fn format(mut self) -> CreatePostDetails<D, U, Formatted> {
+    lazy_static! {
+      static ref RE: Result<Regex, regex::Error> = Regex::new("[^A-Za-z]+");
+    }
+
+    self.topics.iter_mut().for_each(|s| {
+      s.make_ascii_lowercase();
+      s.get_mut(0..1).map(|a| a.make_ascii_uppercase());
+
+      if RE.is_ok() {
+        *s = String::from(RE.as_ref().unwrap().replace_all(s, "").to_string());
+      }
+    });
+
+    CreatePostDetails {
+      title: self.title,
+      topics: self.topics,
+      body: self.body,
+      db_client: self.db_client,
+      user_details: self.user_details,
+      formatted: PhantomData,
+    }
+  }
+}
+
+impl<'a> CreatePostDetails<DBClient<'a>, UserDetails<'a>, Formatted> {
+  pub async fn create_post(&self) -> Result<i32, (StatusCode, Value)> {
     Err((
       StatusCode::INTERNAL_SERVER_ERROR,
-      json!({"message": "Winner"}),
+      json!({ "message": "won" }),
     ))
   }
 }
